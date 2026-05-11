5fed148b1322f4bbc8b67bb8cc0d23390e132093
Moose <basketmati.1@gmail.com>
2026-05-11 21:03:29 +0200

    Dodanie wyświetlania statystki czasu

--- stats.py
+++ stats.py
@@ -1,2 +1,6 @@
-stat =5
-print(stat)
+import time
+
+def print_stats(start_time):
+    elapsed_time = time.time() - start_time
+    print("\n--- STATYSTYKI KOŃCOWE ---")
+    print(f"Czas trwania: {elapsed_time:.2f} sekund")
