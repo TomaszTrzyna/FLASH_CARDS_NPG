054c1c3fe5b442d8778b3c286ee4ba93736ff9f8
Jakub Tabor <jtabor@student.agh.edu.pl>
2026-05-11 21:03:08 +0200

    opis poziomów

---
+++ progress.py
@@ -0,0 +1,5 @@
+def progress_check_mode(repo):
+    print("\nWybierz poziom trudności testu:")
+    print("1. Łatwy (Podstawy)")
+    print("2. Średni (Komunikacja)")
+    print("3. Trudny (Zaawansowany)")
