08949f6753194fd2f87f83fef5896a3666423ad8
Moose <basketmati.1@gmail.com>
2026-05-12 18:39:44 +0200

    Dodano wyświetlanie liczby prób do wyświetlania statystyk (zmienna liczby prób - total)

--- stats.py
+++ stats.py
@@ -1,6 +1,7 @@
 import time
 
-def print_stats(start_time):
+def print_stats(start_time,total):
     elapsed_time = time.time() - start_time
     print("\n--- STATYSTYKI KOŃCOWE ---")
     print(f"Czas trwania: {elapsed_time:.2f} sekund")
+    print(f"Liczba prób: {total}")
