372e1255faffc8d9441eb1e397362b41baed5edc
Jakub Tabor <jtabor@student.agh.edu.pl>
2026-05-14 18:38:00 +0200

    wpisanie przez użytkownika nieprawidlowego poziomu

--- progress.py
+++ progress.py
@@ -3,3 +3,9 @@ def progress_check_mode(repo):
     print("1. Łatwy (Podstawy)")
     print("2. Średni (Komunikacja)")
     print("3. Trudny (Zaawansowany)")
+
+    choice = input("Wybór: ").strip()
+
+    if choice not in ['1', '2', '3']:
+        print("Nieprawidłowy poziom.")
+        return
