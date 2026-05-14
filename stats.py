71bef34c8a5e3e8aaddbf8abaff870ec59de2c2a
Moose <basketmati.1@gmail.com>
2026-05-14 19:36:16 +0200

    Dodano wyświetlanie liczby poprawnych odpowiedzi do wyświetlania statystyk (zmienna liczby prób - correct)

--- stats.py
+++ stats.py
@@ -1,7 +1,9 @@
 import time
 
-def print_stats(start_time,total):
+def print_stats(start_time,correct,total):
     elapsed_time = time.time() - start_time
     print("\n--- STATYSTYKI KOŃCOWE ---")
     print(f"Czas trwania: {elapsed_time:.2f} sekund")
     print(f"Liczba prób: {total}")
+    print(f"Poprawne odpowiedzi: {correct}")
+    print("--------------------------\n")
