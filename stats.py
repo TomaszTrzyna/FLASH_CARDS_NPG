7e65d02143294575e0d3e97adf23ceaa44f3c189
Moose <basketmati.1@gmail.com>
2026-05-15 20:42:08 +0200

    Dodano wyświetlanie skuteczności tzn. ile procent odpowiedzi zostało udzielonych poprawnych (percent - procent poprawnych odpowiedzi)

--- stats.py
+++ stats.py
@@ -2,8 +2,10 @@ import time
 
 def print_stats(start_time,correct,total):
     elapsed_time = time.time() - start_time
+    percent = (correct / total) * 100 if total > 0 else 0
     print("\n--- STATYSTYKI KOŃCOWE ---")
     print(f"Czas trwania: {elapsed_time:.2f} sekund")
     print(f"Liczba prób: {total}")
     print(f"Poprawne odpowiedzi: {correct}")
+    print(f"Skuteczność: {percent:.2f}%")
     print("--------------------------\n")
