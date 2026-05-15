c771f5d7a0793ab23dc55ba0381cdaa2fbf6dc09
TomaszTrzyna <tomasztrzyna@student.agh.edu.pl>
2026-05-15 22:30:34 +0200

    Revert "Struktura klasy Flashcard"

--- models.py
+++
@@ -1,8 +0,0 @@
-class Flashcard:
-    def __init__(self, pl, en, level):
-        self.pl = pl
-        self.en = en
-        self.level = level
-
-    def to_dict(self):
-        return {"pl": self.pl, "en": self.en, "level": self.level}
