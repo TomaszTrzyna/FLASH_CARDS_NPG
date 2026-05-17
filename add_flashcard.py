358f81f21417b2d0ce760513c509872ae0d14c1f
Mateusz Walczak <mati.walczak26@gmail.com>
2026-05-17 17:47:51 +0200

    Dodanie dwukropka na koncu definicji funkcji, bez tego nie dojdzie do kompilacji

--- add_flashcard.py
+++ add_flashcard.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-def add_flashcard_mode(repo)
+def add_flashcard_mode(repo):
     print("\n--- KREATOR NOWEJ FISZKI ---")
     pl = input("Słowo po polsku: ").strip()
     en = input("Słowo po angielsku: ").strip()
