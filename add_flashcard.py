a6187cd6b768cbadeb938aa8d5293d30d22f8b97
Mateusz Walczak <mati.walczak26@gmail.com>
2026-05-19 17:56:26 +0200

    Dodanie wyboru poziomu trudnosci i pierwszego warunku if

--- add_flashcard.py
+++ add_flashcard.py
@@ -4,3 +4,9 @@ def add_flashcard_mode(repo):
     print("\n--- KREATOR NOWEJ FISZKI ---")
     pl = input("Słowo po polsku: ").strip()
     en = input("Słowo po angielsku: ").strip()
+    print("Poziom trudności: 1 (łatwy), 2 (średni), 3 (trudny)")
+    lvl = input("Wybierz poziom: ").strip()
+
+    if pl and en and lvl in ['1', '2', '3']:
+
+        if repo.add(pl, en, int(lvl)):
