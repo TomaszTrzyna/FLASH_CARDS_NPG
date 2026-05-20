8916cf57f680ad318653ad090df5aa0249e17f34
Moose <basketmati.1@gmail.com>
2026-05-20 22:36:59 +0200

    Dodanie licznikow, czas startowego oraz losowanie fiszki

--- survival_mode.py
+++ survival_mode.py
@@ -6,3 +6,6 @@ from stats import print_stats
 def survival_mode(repo):
     all_f = repo.get_all()
     flashcards = list(all_f)
+    random.shuffle(flashcards)
+    correct, total, mistakes = 0, 0, 0
+    start_time = time.time()
